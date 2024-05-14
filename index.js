127e9f8e570477c57d99835cb32a7845973475ba
jaanu-setti <jaanusetti03@gmail.com>
2024-05-14 14:40:44 +0530

    delete req added

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const app = express();
 app.get('/',(req,res)=>{
     res.send("hello everyone")
 })
+
+app.delete('/',(req,res)=>{
+    res.send("iam from delete req")
+})
 app.listen(3000,()=>console.log("server is running at 3000"))
