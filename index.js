e3ce93180ce8926eb57dbe46d369f88305c2d6bd
jaanu-setti <jaanusetti03@gmail.com>
2024-05-14 14:36:57 +0530

    post request added

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const app = express();
 app.get('/',(req,res)=>{
     res.send("hello everyone")
 })
+app.post('/',(req,res)=>{
+    res.send("iam from post request")
+})
 app.listen(3000,()=>console.log("server is running at 3000"))
